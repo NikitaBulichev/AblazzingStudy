991a2ae0a406c0c9cb7acbf141d89aac3a88aecb
NikitaBulichev <potti352085@gmail.com>
2022-07-20 22:10:20 +0500

    Решена первая задача

--- src/AdvancedHomework2/Advanced2.java
+++ src/AdvancedHomework2/Advanced2.java
@@ -2,4 +2,27 @@ package AdvancedHomework2;
 
 public class Advanced2 {
 
+    public static void main(String[] args) {
+
+
+        // Задание №1: Написать цикл, который будет прибавлять число к result до тех пор,
+        // пока не получиться больше 1_000_000.
+        // Дано:
+        double increment = 0.01123;
+        double result = 0;
+        // Вывести на экран, количество итераций, которое потребовалось, чтобы дойти до миллиона.
+        // Если число отрицательное, то сразу заканчиваем цикл, ничего не выводя.
+        // Внимание: число может измениться, и не должно приводить к изменению вашего кода.
+        // РЕШЕНИЕ
+
+        int count = 0;
+        if (result < 0) {
+        } else {
+            while (result < 1000000) {
+                count++;
+                result = result + increment;
+            }
+            System.out.println(count);
+        }
+    }
 }
