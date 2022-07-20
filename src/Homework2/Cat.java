b6400c879a0e5d4a5ae8cccbf940b247777184df
NikitaBulichev <potti352085@gmail.com>
2022-07-20 21:52:11 +0500

    Решены четыре задачи

---
+++ src/Homework2/Cat.java
@@ -0,0 +1,31 @@
+package Homework2;
+
+public class Cat {
+
+    String color;
+
+    int age;
+
+    public Cat(String color, int age) {
+
+        this.color = color;
+
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/Homework2/Dog.java
@@ -0,0 +1,31 @@
+package Homework2;
+
+public class Dog {
+
+    String eyecolor;
+
+    int age;
+
+    public Dog(String eyecolor, int age) {
+
+        this.eyecolor = eyecolor;
+
+        this.age = age;
+    }
+
+    public String getEyecolor() {
+        return eyecolor;
+    }
+
+    public void setEyecolor(String eyecolor) {
+        this.eyecolor = eyecolor;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
