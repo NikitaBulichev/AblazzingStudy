8f4b7be3e838cfbae5f119aaafc5e61d6940d087
NikitaBulichev <potti352085@gmail.com>
2022-07-28 14:35:35 +0500

    Решена вторая продвинутая

--- src/Homework3/Homework3.java
+++ src/Homework3/Homework3.java
@@ -1,10 +1,9 @@
 package Homework3;
 
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Random;
 
 public class Homework3 {
     public static void main(String[] args) throws IOException {
@@ -27,7 +26,7 @@ public class Homework3 {
 
         Vaz samara = new Vaz();
 
-        Vaz bogdan = new Vaz();
+        Vaz vedro = new Vaz();
 
         camry.musicOn();
 
@@ -35,7 +34,7 @@ public class Homework3 {
 
         samara.crash();
 
-        bogdan.crash();
+        vedro.crash();
 
         //Задача №2
         //Необходимо:
@@ -45,7 +44,7 @@ public class Homework3 {
 
         // РЕШЕНИЕ
 
-        String filePath = "D:\\Programming\\Java\\AblazzingStudy\\src\\Homework3\\my_firs_file.txt";
+        String filePath = "D:\\Programming\\Java\\AblazzingStudy\\src\\Homework3\\Resourses\\my_firs_file.txt";
 
         FileWriter fileWriter = new FileWriter(filePath);
 
@@ -63,7 +62,7 @@ public class Homework3 {
             while (((line = bufferedReader.readLine()) != null)) {
                 result = result.concat(line + "");
             }
-            System.out.print(result);
+            System.out.println(result);
         }
 
 
@@ -79,7 +78,7 @@ public class Homework3 {
 
         FinancialRecord record = new FinancialRecord(500, 300);
 
-        String reportPath = "D:\\Programming\\Java\\AblazzingStudy\\src\\Homework3\\report.txt";
+        String reportPath = "D:\\Programming\\Java\\AblazzingStudy\\src\\Homework3\\Resourses\\report.txt";
 
         FileWriter fileWriter1 = new FileWriter(reportPath);
 
@@ -112,5 +111,52 @@ public class Homework3 {
                 ((Toyota) car).musicOn();
             }
         }
+
+        Random random = new Random(1);
+        // random.nextInt(10000);
+        //Задача №2
+        // 1. Создать класс Financial record, с двумя атрибутами: incomes, outcomes (доходы, расходы)
+        // 2. Создать в этом классе геттеры, сеттеры и конструктор на все атрибуты
+        // 3. Создать 10 отчетов, с разными доходами и расходами (используй random)
+        // 4. Записать в файл "report.txt" все данные из отчетов.
+        // 5. Прочитать файл report.txt, просуммировать все доходы и вывести на экран, тоже самое с расходами
+        // Ожидаемый результат: общие доходы - (какое то число), общие расходы - (какое то число)
+
+        //РЕШЕНИЕ
+
+        int finalIncomes = 0;
+        int finalOutcomes = 0;
+
+        FinancialRecord[] financialRecords = new FinancialRecord[10];
+        for (int i = 0; i < financialRecords.length; i++) {
+            int j = random.nextInt(10000);
+            int k = random.nextInt(10000);
+            financialRecords[i] = new FinancialRecord(j,k);
+        }
+       // System.out.println(Arrays.toString(financialRecords));
+
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(reportPath));
+
+        String mathResult = "";
+
+        for (FinancialRecord math : financialRecords) {
+            mathResult += math.toString() + "\n";
+        }
+        mathResult = mathResult.substring(0, mathResult.length() - 1);
+        bufferedWriter.write(mathResult);
+        bufferedWriter.close();
+
+        BufferedReader bufferedReader1 = new BufferedReader(new FileReader(reportPath));
+        while (bufferedReader1.ready()){
+
+            String string = bufferedReader1.readLine();
+            String[] strings = string.split(":");
+            int income = Integer.parseInt(strings[0]);
+            int outcome = Integer.parseInt(strings[1]);
+            finalIncomes += income;
+            finalOutcomes += outcome;
+        }
+        bufferedReader1.close();
+        System.out.println("Общие доходы - " + finalIncomes + ", общие расходы - " + finalOutcomes);
     }
 }
